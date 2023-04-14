72d6d0bb5a771a4ca1bd75f7e2f2d831b5922939
Vlad <plotnikov.va2004@gmail.com>
2023-04-14 16:35:39 +0400

    Готовый главный перс

---
+++ Assets/Person/Pers/Main/Scripts/playerGun.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerGun : MonoBehaviour
+{
+
+    public Transform body;
+    public bool hasWeap;
+    private GameObject myGun;
+    public grab leftHand, rightHand;
+    public arms leftArm;
+    public camera cam;
+
+    private void Update()
+    {
+
+        if (hasWeap)
+        {
+            leftHand.canGrab = false; rightHand.canGrab = false;
+            leftArm.canUse = false;
+            if (Input.GetMouseButton(1))
+            {
+                cam.aiming = true;
+            }
+            else
+            {
+                cam.aiming = false;
+            }
+        }
+        else
+        {
+            leftHand.canGrab = true; rightHand.canGrab = true;
+            leftArm.canUse = true;
+            cam.aiming = false;
+        }
+
+        if (cam.aiming)
+        {
+            gun myGunsScript = myGun.GetComponent<gun>();
+            if (myGunsScript.oneShotAtATime)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    myGunsScript.shoot(null);
+                }
+            }
+            else
+            {
+                if (Input.GetMouseButton(0))
+                {
+                    myGunsScript.shoot(null);
+                }
+            }
+        }
+    }
+
+}
