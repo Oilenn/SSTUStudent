78a091f5e378b45158f43edce487648f7f5dc63c
Rostislav Elengaupt <grandluntik0@gmail.com>
2023-04-14 18:41:49 +0400

    Постройка кабинета математики

--- Assets/Scripts/Quests/FirstQuest.cs
+++ Assets/Scripts/Quests/FirstQuest.cs
@@ -22,8 +22,8 @@ public class FirstQuest : MonoBehaviour
     */
 
     //Объект диалогового окна.
-    [Header("Phrases")]
-    [SerializeField] private List<string> Andrey = new List<string>();
+    [Header("Phrases. The first element must to be name of a character.")]
+    [SerializeField] private List<string> MathWoman = new List<string>();
     [SerializeField] private List<string> Nikita = new List<string>();
 
     [Header("")]
@@ -45,8 +45,11 @@ public class FirstQuest : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E) && !_window.activeSelf)
         {
-            Queue<string> phrases = new Queue<string>(Andrey);
-            StartDialog("Андрей", phrases);
+            Queue<string> phrases = new Queue<string>(MathWoman);
+            //Первый элемент в очереди - имя персонажа, с кем будет вестись диалог.
+            StartDialog(phrases.Dequeue(), phrases);
         }
     }
+
+
 }
